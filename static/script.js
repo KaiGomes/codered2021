35078890bc2395b94fd9f71c9e65e90482bc0270
Kai Gomes <kaigomes7@tamu.edu>
2021-10-03 11:41:27 -0500

    Added stuff

--- static/script.js
+++ static/script.js
@@ -2,110 +2,116 @@ var directionsService = new google.maps.DirectionsService();
 var map;
 
 function initialize() {
-
   var center = new google.maps.LatLng(0, 0);
   var myOptions = {
     zoom: 7,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: center
-  }
+    center: center,
+  };
 
   map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 
-
-  var start = prompt ("Enter start point: ", "16026 Green Manor Drive, Houston, TX");
+  var start = prompt("Enter start point: ", "16026 Green Manor Drive, Houston, TX");
   var end = prompt("Enter destination", "4800 Calhoun Road, Houston, TX");
-
-  fetch(`/routes/${start}/${end}`)
-  .then(response => response.json())
-  .then(json => {
-    // var parser = new DOMParser();;
-    // var doc = parser.parseFromString(html, "text/html");
-    var val = JSON.stringify(json);
-    // doc.querySelector('scores').innerHTML = json;
-    document.getElementById('scores').innerHTML = val;
-    console.log('GET response:');
-    console.log(val);
-});
 
   plotDirections(start, end);
 }
 
 function plotDirections(start, end) {
-
-  var method = 'DRIVING';
+  var method = "DRIVING";
 
   var request = {
     origin: start,
     destination: end,
     travelMode: google.maps.DirectionsTravelMode[method],
-    provideRouteAlternatives: true
+    provideRouteAlternatives: true,
   };
+  var val;
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-  directionsService.route(request, function(response, status) {
+  async function getData () {
+  fetch(`/routes/${start}/${end}`)
+    .then((response) => response.json())
+    .then((json) => {
+      // var parser = new DOMParser();;
+      // var doc = parser.parseFromString(html, "text/html");
+      val = JSON.stringify(json);
+      // doc.querySelector('scores').innerHTML = json;
+      document.getElementById("scores").innerHTML = val;
+      // await sleep(2000);
+      console.log("GET response:");
+      console.log(val);
+    });
+  }
+  getData();
 
+  directionsService.route(request, function (response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
-
       var routes = response.routes;
-      var colors = ['red', 'green', 'blue', 'orange', 'yellow', 'black'];
+      var colors = ["red", "green", "blue", "orange", "yellow", "black"];
       var directionsDisplays = [];
 
       // Reset the start and end variables to the actual coordinates
       start = response.routes[0].legs[0].start_location;
       end = response.routes[0].legs[0].end_location;
 
-			// Loop through each route
+      // Loop through each route
       for (var i = 0; i < routes.length; i++) {
-        var directionsDisplay = new google.maps.DirectionsRenderer({
-          map: map,
-          directions: response,
-          routeIndex: i,
-          draggable: true,
-          polylineOptions: {
+        var directionsDisplay = new google.maps.DirectionsRenderer(
+          {
+            map: map,
+            directions: response,
+            routeIndex: i,
+            draggable: true,
+            polylineOptions: {
+              strokeColor: colors[i],
+              strokeWeight: 4,
+              strokeOpacity: 0.3,
+            },
+          },
+          false
+        );
 
-            strokeColor: colors[i],
-            strokeWeight: 4,
-            strokeOpacity: .3
-          }}, false)
+        // const routeVar = document.getElementsByClassName("route");
+        // for (let j = 0; j < routes.length; j++) {
+        //   routeVar[j].setAttribute("id", j);
+        //   document.getElementById(j).innerHTML = 'Route<span class="numSpan"></span>';
+        // }
 
-      const routeVar = document.getElementsByClassName("route");
-      for (let j = 0; j < routes.length; j++) {
-        routeVar[j].setAttribute('id', j);
-        document.getElementById(j).innerHTML = 'Route<span class="numSpan"></span>';
-      }
-
-      for (let j = 0; j < routes.length; j++) {
-        const spanVar = document.getElementsByClassName("numSpan");
-        routeVar[j].setAttribute('id', 'none');
-        spanVar[j].setAttribute('id',j);
-        document.getElementById(j).innerHTML =" " + (j + 1) + "Append JSON here?";
-      }
+        // for (let j = 0; j < routes.length; j++) {
+        //   const spanVar = document.getElementsByClassName("numSpan");
+        //   routeVar[j].setAttribute("id", "none");
+        //   spanVar[j].setAttribute("id", j);
+        //   document.getElementById(j).innerHTML = " " + (j + 1) + val;
+        // }
 
         // Push the current renderer to an array
         directionsDisplays.push(directionsDisplay);
 
         // Listen for the directions_changed event for each route
-        google.maps.event.addListener(directionsDisplay, 'directions_changed', (function(directionsDisplay, i) {
+        google.maps.event.addListener(
+          directionsDisplay,
+          "directions_changed",
+          (function (directionsDisplay, i) {
+            return function () {
+              var directions = directionsDisplay.getDirections();
+              var new_start = directions.routes[0].legs[0].start_location;
+              var new_end = directions.routes[0].legs[0].end_location;
 
-          return function() {
+              if (new_start.toString() !== start.toString() || new_end.toString() !== end.toString()) {
+                // Remove every route from map
+                for (var j = 0; j < directionsDisplays.length; j++) {
+                  directionsDisplays[j].setMap(null);
+                }
 
-            var directions = directionsDisplay.getDirections();
-            var new_start = directions.routes[0].legs[0].start_location;
-            var new_end = directions.routes[0].legs[0].end_location;
-
-            if ((new_start.toString() !== start.toString()) || (new_end.toString() !== end.toString())) {
-
-              // Remove every route from map
-              for (var j = 0; j < directionsDisplays.length; j++) {
-
-                directionsDisplays[j].setMap(null);
+                // Redraw routes with new start/end coordinates
+                plotDirections(new_start, new_end);
               }
-
-              // Redraw routes with new start/end coordinates
-              plotDirections(new_start, new_end);
-            }
-          }
-        })(directionsDisplay, i)); // End listener
+            };
+          })(directionsDisplay, i)
+        ); // End listener
       } // End route loop
     }
   });
